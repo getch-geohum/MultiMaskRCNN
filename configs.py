288d2df420ed7b81c66594788beb21493770a27e
Getachew Workineh Gella <82418348+getch-geohum@users.noreply.github.com>
2025-02-06 00:46:05 +0100

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,8 +2,8 @@
 #
 # }
 
-train_configs = {"save_dir":"/home/getch/ssl/MaskRCNN_output/model_final",   # directory to save file
-                 "root":"/home/getch/DATA/VAE/data",       # data root where training data is saved
+train_configs = {"save_dir":"/MaskRCNN_output/model_final",   # directory to save file
+                 "root":"/data",       # data root where training data is saved
                  "lr":0.001,
                  "epochs":50,
                  "lr_scheduler":None,
@@ -21,10 +21,10 @@ train_configs = {"save_dir":"/home/getch/ssl/MaskRCNN_output/model_final",   # d
                  "col":256
                  }
 
-predict_configs = {"save_dir": "/home/getch/ssl/MaskRCNN_output/preds_final_check",
-                   "data_dir": "/home/getch/DATA/VAE/data/Minawao_june_2016",
-                   "weight": "/home/getch/ssl/MaskRCNN_output/model_final/model_model_first_best_weight.pt",
-                   "model": "/home/getch/ssl/MaskRCNN_output/model_model_first.pt",
+predict_configs = {"save_dir": "/MaskRCNN_output/preds_final_check",
+                   "data_dir": "/data/Minawao_june_2016",
+                   "weight": "/MaskRCNN_output/model_final/model_model_first_best_weight.pt",
+                   "model": "/MaskRCNN_output/model_model_first.pt",
                    "name": "minawao", # name to save prediction as shapefile
                    "mask_treshold": 0.5,
                    "score_treshold": 0.6,
@@ -35,8 +35,8 @@ predict_configs = {"save_dir": "/home/getch/ssl/MaskRCNN_output/preds_final_chec
                    "col":256
                    }
 
-finetune_configs = {"save_dir":"/home/getch/ssl/MaskRCNN_output/model_tuned",
-                    "root":"/home/getch/DATA/VAE/data",
+finetune_configs = {"save_dir":"/MaskRCNN_output/model_tuned",
+                    "root":"/data",
                     "lr":0.0001,
                     "epochs":50,
                     "train_batch":12,
@@ -49,7 +49,7 @@ finetune_configs = {"save_dir":"/home/getch/ssl/MaskRCNN_output/model_tuned",
                     "train_ratio":0.2,
                     "reserve_test_sample":True,
                     "petience":10,
-                    "weight":"/home/getch/ssl/MaskRCNN_output/model/model_model_first_best_weight.pt",
+                    "weight":"/MaskRCNN_output/model/model_model_first_best_weight.pt",
                     "row":256,
                     "col":256
                     }
